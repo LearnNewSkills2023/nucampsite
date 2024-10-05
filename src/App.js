9c69fa0ee3e4312ac2f367e94b163dd6ea253c62
Lashod Jones <lashodj@gmail.com>
2024-10-05 17:13:05 -0400

    Comments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import { Routes, Route } from 'react-router-dom'
+import ContactPage from './pages/ContactPage'
+import HomePage from './pages/HomePage'
 import Header from './components/Header.js'
 import Footer from './components/Footer.js'
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage.js'
+import CampsiteDetailPage from './pages/CampsiteDetailPage.js'
+import AboutPage from './pages/AboutPage.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />      
+      <Header /> 
+      <Routes>
+        <Route path='/' element ={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='about' element={<AboutPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route
+        path='directory/:campsiteId'
+        element={<CampsiteDetailPage/>} 
+        />
+      </Routes>     
       <CampsitesDirectoryPage />
       <Footer />
     </div>
---
+++ src/features/partners/partnersSlice.js
@@ -0,0 +1,10 @@
+import { PARTNERS } from '../../app/shared/PARTNERS'
+
+export const selectPartners = () => {
+    return PARTNERS; 
+}
+
+export const selectFeaturedPartner = () => {
+    
+    return PARTNERS.find((partner) => partner.featured);
+}
